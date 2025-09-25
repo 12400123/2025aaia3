5b1c54f8b696d445e17c2943f76cc467593ad82d
12400123 <12400123@me.mcu.edu.tw>
2025-09-25 17:18:42 +0800

    Add files via upload

---
+++ week03-2a.cpp
@@ -0,0 +1,18 @@
+//week03-2a.cpp二合一
+//LeetCode 學習計畫第八題 1822. Sign of the Product of an Array
+// 把陣列乘起來 看是正數 負數 還是0
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) 
+    {
+        int ans =1;//因為0乘任何數 都會變成0 只有乘1 乘甚麼 變甚麼
+        for (int i=0;i<nums.size(); i++)//看有幾個數 迴圈跑幾次
+        {
+            ans*=nums[i];//每次把nums[i]乘進ans裡
+        }//數字越乘越大 1000個數字 乘到一半就爆炸了
+        if(ans>0) return 1;
+        if(ans<0) return -1;
+        return 0;
+    }
+};
